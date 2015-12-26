54ac19d7cd439fcba7f5d3d5b7b6c79b80eed6ba
restart32 <kiarashbikar@gmail.com>
2015-12-25 16:46:05 -0800

    Sort File

---
+++ app/src/main/java/com/restart/stocklistener/FooBar.java
@@ -0,0 +1,7 @@
+package com.restart.stocklistener;
+
+/**
+ * Created by Restart on 12/25/2015.
+ */
+public class Sortmanager {
+}
---
+++ app/src/main/java/com/restart/stocklistener/SortManager.java
@@ -0,0 +1,27 @@
+package com.restart.stocklistener;
+
+
+import android.widget.Button;
+
+public class SortManager {
+    Button [] button;
+
+    public SortManager(Button[] button) {
+        this.button = button;
+    }
+
+    public Button [] Sortsymbol() {
+
+        return button;
+    }
+
+    public Button [] Sortprice() {
+
+        return button;
+    }
+
+    public Button [] Sortchange() {
+
+        return button;
+    }
+}
